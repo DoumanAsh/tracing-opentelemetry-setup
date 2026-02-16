05ffa3665811f78718373d2d4def78e1b08baf11
DoumanAsh <douman@gmx.se>
2026-02-16 14:53:00 +0900

    Add option to customize panic recorder

--- src/panic.rs
+++ src/panic.rs
@@ -5,6 +5,64 @@ use std::panic::PanicHookInfo;
 use std::sync::OnceLock;
 use std::backtrace::{Backtrace, BacktraceStatus};
 
+///Describes how to record panic
+pub trait PanicRecorder {
+    ///Performs capture of the panic as event
+    fn capture(location: &Location, message: &str, backtrace: Backtrace);
+}
+
+///Instructs to record panics as opentelemetry [exception](https://opentelemetry.io/docs/specs/semconv/exceptions/exceptions-spans/)
+pub struct Exception;
+impl PanicRecorder for Exception {
+    #[track_caller]
+    fn capture(location: &Location, message: &str, backtrace: Backtrace) {
+        if let BacktraceStatus::Captured = backtrace.status() {
+            tracing::error!(
+                exception.location = %location,
+                exception.stacktrace = %backtrace,
+                exception.message = message,
+                exception.type = "Rust Panic",
+                "exception",
+            );
+        } else {
+            tracing::error!(
+                exception.location = %location,
+                exception.message = message,
+                exception.type = "Rust Panic",
+                "exception",
+            );
+        }
+    }
+}
+
+///Instructs to record panics as opentelemetry [error](https://opentelemetry.io/docs/specs/semconv/registry/attributes/error/#error-attributes) attribute
+///
+///In addition to that it records stacktrace as `error.stack` following [datadog conventions](https://docs.datadoghq.com/tracing/error_tracking/#use-span-attributes-to-track-error-spans)
+///
+///Prefer this if you use datadog as target
+pub struct Error;
+impl PanicRecorder for Error {
+    #[track_caller]
+    fn capture(location: &Location, message: &str, backtrace: Backtrace) {
+        if let BacktraceStatus::Captured = backtrace.status() {
+            tracing::error!(
+                error.location = %location,
+                error.stack = %backtrace,
+                error.message = message,
+                error.type = "Rust Panic",
+                "exception",
+            );
+        } else {
+            tracing::error!(
+                error.location = %location,
+                error.message = message,
+                error.type = "Rust Panic",
+                "exception",
+            );
+        }
+    }
+}
+
 fn propagate_status(message: &str) {
     use opentelemetry::trace::Status;
     use tracing_opentelemetry::OpenTelemetrySpanExt;
@@ -19,6 +77,11 @@ fn propagate_status(message: &str) {
 
 ///Panic hook implementation
 pub fn panic_hook(panic: &PanicHookInfo<'_>) {
+    panic_hook_as::<Exception>(panic)
+}
+
+///Panic hook implementation with customized recorder
+pub fn panic_hook_as<P: PanicRecorder>(panic: &PanicHookInfo<'_>) {
     const DEFAULT_MESSAGE: &'static str = "panic occurred";
 
     let location = match panic.location() {
@@ -35,32 +98,22 @@ pub fn panic_hook(panic: &PanicHookInfo<'_>) {
 
     propagate_status(msg);
     let backtrace = Backtrace::force_capture();
-    if let BacktraceStatus::Captured = backtrace.status() {
-        tracing::error!(
-            exception.location = %location,
-            exception.stacktrace = %backtrace,
-            exception.message = msg,
-            exception.type = "Rust Panic",
-            "exception",
-        );
-    } else {
-        tracing::error!(
-            exception.location = %location,
-            exception.message = msg,
-            exception.type = "Rust Panic",
-            "exception",
-        );
-    }
+    P::capture(location, msg, backtrace);
 }
 
 ///Installs [panic_hook] once
 pub fn install_panic_hook() {
+    install_panic_hook_as(Exception)
+}
+
+///Installs [panic_hook] once with specified [PanicRecorder]
+pub fn install_panic_hook_as<T: PanicRecorder>(_: T) {
     static ONCE: OnceLock<()> = OnceLock::new();
 
     ONCE.get_or_init(|| {
         let next = std::panic::take_hook();
         std::panic::set_hook(Box::new(move |info| {
-            panic_hook(info);
+            panic_hook_as::<T>(info);
             next(info);
         }));
     });
